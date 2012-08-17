57dee21172ce3d136d0975b6f9a014c5ed115780
Jason Wertz <jason.wertz@gmail.com>
2012-08-17 14:13:52 -0400

    Initial Commit
    General game framework established.
    
    Basic seesaw functionality implemented.

---
+++ Assets/Scripts/HUD.js
@@ -0,0 +1,32 @@
+#pragma strict
+
+// GUI Style
+var scoreStyle : GUIStyle;
+@HideInInspector
+var scoreSize : Vector2;
+
+// Text padding values
+var topPadding = .05;
+var leftPadding = .05;
+var rightPadding = .05;
+
+function Start () {
+
+	// Measure to get max size
+	scoreSize = scoreStyle.CalcSize(new GUIContent("88"));
+}
+
+function Update () {
+
+}
+
+function OnGUI() {
+	// Left player score
+	GUI.Label(new Rect(Screen.width*leftPadding,Screen.height*topPadding,scoreSize.x,scoreSize.y),"88",scoreStyle);
+
+
+	// Right player score
+	GUI.Label(new Rect((Screen.width*(1.0-leftPadding)-scoreSize.x),Screen.height*topPadding,scoreSize.x,scoreSize.y),"88",scoreStyle);
+
+
+}
---
+++ Assets/Scripts/ScoreTrigger.js
@@ -0,0 +1,17 @@
+#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(other : Collider) {
+	
+// Score
+		Debug.Log("Score");
+		Debug.Log(other);
+
+}
